d9eae270b0c4dba9c7ea345be1ecfe37c9bb6ed8
gratinomaster <contato@guilhermemartins.tv>
2026-06-12 01:25:31 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -1,18 +1,3 @@
-import requests
-
-url = "https://drive.usercontent.google.com/u/0/uc?id=1-FfnDKu_NpGSH_r2HTVMRvMZW2-miBbf&export=download"
-arquivo_destino = "lista1.M3U"
-
-resposta = requests.get(url)
-resposta.raise_for_status()
-
-with open(arquivo_destino, "wb") as arquivo:
-    arquivo.write(resposta.content)
-
-print("Download concluído com sucesso!")
-
-
-
 import os
 import requests
 import re
